4b135a44ee914ffed5350f6431d0eab85d273467
Veasna Reaksa <veasnareaksa155@gmail.com>
2026-09-10 15:10:15 +0700

    Force HTTPS for Render mixed content

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,24 +2,20 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        Vite::prefetch(concurrency: 3);
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
